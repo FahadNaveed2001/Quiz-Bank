8221f4f5217c4d3fdeefd4fa9ea1d5a5668b8e85
FahadNaveed2001 <fahadshelby2001@gmail.com>
2024-05-20 22:59:44 +0500

    corse origin changed

--- Index.js
+++ Index.js
@@ -60,9 +60,11 @@ const PORT = process.env.PORT || 8000;
 
 //db connection
 connectDB();
+
 //cors
 const corsOptions = {
   origin: [
+    "*",
     "https://zap70.com",
     "http://localhost:3000",
     "http://165.232.134.133:3000",
@@ -73,11 +75,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-//app uses
-// app.use(express.json());
-// app.use(cors());
-
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -89,6 +86,9 @@ app.use(
   "/uploads/videos",
   express.static(path.join(__dirname, "uploads", "videos"))
 );
+// app uses
+// app.use(express.json());
+// app.use(cors());
 
 //Jwt Secret
 const JWT_SECRET = process.env.JWT_SECRET_KEY;
@@ -113,7 +113,10 @@ app.get("/", (req, res) => {
 });
 
 //multer storage
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 100 * 1024 * 1024 },
+});
 
 //mcq routes
 app.post(
@@ -281,6 +284,18 @@ const deleteVideo = (filename) => {
       console.error("Error deleting video:", err);
     } else {
       console.log("Video deleted successfully.");
+    }
+  });
+};
+
+const addExcel = (filename) => {
+  const excelpath = path.join(__dirname, "uploads", "excels", filename);
+
+  fs.unlink(excelpath, (err) => {
+    if (err) {
+      console.error("error while adding mcq", err);
+    } else {
+      console.log("mcq loaded succesfully");
     }
   });
 };
